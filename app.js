c7ac95b5ccb2266b68adf1cd281b2accf88060f7
Kevin Mahoney <kmahoney@theatlantic.com>
2018-10-16 17:50:36 -0400

    render project data

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const ejs = require('ejs');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = process.env.PORT || 4001;
 
+//TODO: Extrapolate out as a 'model'
+const getProjects = async () => {
+  const projects = await fetch('https://aqueous-waters-18637.herokuapp.com/api/projects').then((res) => {
+    return res.json();
+  }).then((projectsJson) => {
+    return projectsJson;
+  })
+  return projects;
+}
+
+//TODO: Extrapolate out as a 'controller'
+const loadPage = async function (req, res) {
+  const projects = await getProjects();
+  res.render('base', { projects: JSON.stringify(projects)})
+}
 
 app.engine('.html', ejs.__express);
 app.set("view engine", ".html")
@@ -13,6 +29,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.render('base', {}));
+app.get('/', loadPage);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ public/js/projects.js
@@ -0,0 +1,10 @@
+// console.log('projects');
+//
+// const getProjects = (res) => {
+//   fetch('https://aqueous-waters-18637.herokuapp.com/api/projects').then(() => {
+//     return res.json();
+//   }).then((projectsJson) => {
+//     console.log(JSON.stringify(projectsJson));
+//   })
+// }
+// getProjects();
